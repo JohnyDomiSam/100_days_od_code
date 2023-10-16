21e38529ae1945027c5208a8cd0aee4ba3197758
Jan Kubinec <kubinec.jan@gmail.com>
2023-10-16 23:31:12 +0200

    add final 36

--- 36.Stock_news_app/stock-news-extrahard-start/main.py
+++ 36.Stock_news_app/stock-news-extrahard-start/main.py
@@ -1,5 +1,7 @@
 import requests
+from twilio.rest import Client
 
+DIFF = 0
 # Alphavantage API info
 STOCK = "TSLA"
 COMPANY_NAME = "Tesla Inc"
@@ -16,49 +18,34 @@ news_parameters = {"q": "Tesla",
                    "category": "business",
                    "country": "us",
                    "apiKey": "f9c8979709224cb7b19fa2b77c8f8ba9"}
-response = requests.get(url=NEWS_URL, params=news_parameters)
-response.raise_for_status()
-data = response.json()
-title = data["articles"][0]["title"]
-article = data["articles"][0]["description"]
-print(article)
+
+# Twilio info
+account_sid = 'AC89d90d3e2bc457fe9433e12f1d26f32d'
+auth_token = "auth_token"
+print(auth_token)
 
 
-def get_news():
-    response = requests.get(url=URL, params=parameters)
+def get_move():
+    response_ts = requests.get(url=URL, params=parameters)
+    response_ts.raise_for_status()
+    data_ts = response_ts.json()
+    date_list = list(data_ts["Time Series (Daily)"])
+    y_close = float(data_ts["Time Series (Daily)"][date_list[0]]["4. close"])
+    b_close = float(data_ts["Time Series (Daily)"][date_list[1]]["4. close"])
+    global DIFF
+    DIFF = 100 * round(((y_close - b_close) / ((y_close + b_close) / 2)), 2)
+    return DIFF
+
+
+if get_move() > 3 or get_move() < -3:
+    response = requests.get(url=NEWS_URL, params=news_parameters)
     response.raise_for_status()
     data = response.json()
-    date_list = list(data["Time Series (Daily)"])
-    y_close = float(data["Time Series (Daily)"][date_list[0]]["4. close"])
-    b_close = float(data["Time Series (Daily)"][date_list[1]]["4. close"])
-    diff = 100 * round(((y_close - b_close)/((y_close + b_close)/2)), 2)
-    if diff > 5:
-        return f"{diff} %, big move !"
-    else:
-        return f"{diff} %, small move"
-
-
-
-
-
-## STEP 1: Use https://www.alphavantage.co
-# When STOCK price increase/decreases by 5% between yesterday and the day before yesterday then print("Get News").
-
-## STEP 2: Use https://newsapi.org
-# Instead of printing ("Get News"), actually get the first 3 news pieces for the COMPANY_NAME. 
-
-## STEP 3: Use https://www.twilio.com
-# Send a seperate message with the percentage change and each article's title and description to your phone number.
-
-
-#Optional: Format the SMS message like this: 
-"""
-TSLA: 🔺2%
-Headline: Were Hedge Funds Right About Piling Into Tesla Inc. (TSLA)?. 
-Brief: We at Insider Monkey have gone over 821 13F filings that hedge funds and prominent investors are required to file by the SEC The 13F filings show the funds' and investors' portfolio positions as of March 31st, near the height of the coronavirus market crash.
-or
-"TSLA: 🔻5%
-Headline: Were Hedge Funds Right About Piling Into Tesla Inc. (TSLA)?. 
-Brief: We at Insider Monkey have gone over 821 13F filings that hedge funds and prominent investors are required to file by the SEC The 13F filings show the funds' and investors' portfolio positions as of March 31st, near the height of the coronavirus market crash.
-"""
-
+    title = data["articles"][0]["title"]
+    article = data["articles"][0]["description"]
+    client = Client(account_sid, auth_token)
+    message = client.messages.create(
+        body=f"TSLA: {DIFF}%\nHeadline: {title}\nBrief: {article}",
+        from_='+18182931791',
+        to='+421905993795'
+    )
